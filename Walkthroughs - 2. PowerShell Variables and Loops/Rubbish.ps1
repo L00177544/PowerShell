cb55d19d16837f9cca5286758a18510150fa956f
L00177544 <L00177544@atu.ie>
2022-12-04 17:45:21 +0000

    Commit nr 4

--- Walkthroughs - 2. PowerShell Variables and Loops/Rubbish.ps1
+++ Walkthroughs - 2. PowerShell Variables and Loops/Rubbish.ps1
@@ -3,3 +3,18 @@ $Rubbish
 clear-variable -Name Rubbish
 $Rubbish
 Remove-Variable -Name Rubbish
+
+$Rubbish = 1, 2, "a", "££"
+$Rubbish.GetType()
+
+[int]$Rubbish = 1
+$Rubbish.GetType()
+
+[int]$Rubbish = 1
+$Rubbish = "123456789"
+$Rubbish
+
+[int]$Rubbish = 1
+$Rubbish = "This will give you an error!"
+$Rubbish
+
---
+++ Walkthroughs - 2. PowerShell Variables and Loops/types.ps1
@@ -0,0 +1,12 @@
+$StringValue = "Yoo hoo!"
+$StringValue.ToUpper()
+$StringValue.ToLower()
+
+$MyArray = 1,2,3,4,5
+$MyArray[1]
+
+$LittleNumber = 12345
+$LittleNumber.GetType()
+$BigNumber = 123456789123456789
+$BigNumber.GetType()
+
